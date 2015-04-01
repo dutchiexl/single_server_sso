9f40bd9cea2ca24fc420845c9fffa9750b468d31
Neal Vanmeert <neal@emuse.be>
2015-04-01 14:48:50 +0200

    First working version.

--- js/cgk_sso.js
+++ js/cgk_sso.js
@@ -1,11 +1,11 @@
 (function ($) {
     Drupal.behaviors.cgk_sso = {
         attach: function (context, settings) {
-            console.log(Drupal.settings.cgk_sso);
             $.each(Drupal.settings.cgk_sso.callback_urls, function (key, callback_url) {
-                console.log('Key: ' + key + "Value: " + callback_url);
-                $.post(callback_url + "/" + Drupal.settings.cgk_sso.uid + "/4", function (data) {
-
+                $.ajax({
+                    url: callback_url + "/" + Drupal.settings.cgk_sso.uid + "/4",
+                    type: "GET",
+                    dataType: 'jsonp'
                 });
             });
         }
