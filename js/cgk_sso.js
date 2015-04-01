c03972d31f815601961596128b437c27e685d0ad
Neal Vanmeert <neal@emuse.be>
2015-04-01 12:48:12 +0200

    Created callback function in JS.

--- js/cgk_sso.js
+++ js/cgk_sso.js
@@ -1,7 +1,13 @@
 (function ($) {
     Drupal.behaviors.cgk_sso = {
         attach: function (context, settings) {
-            alert(Drupal.settings.cgk_sso);
+            console.log(Drupal.settings.cgk_sso);
+            $.each(Drupal.settings.cgk_sso.callback_urls, function (key, callback_url) {
+                console.log('Key: ' + key + "Value: " + value);
+                $.post(callback_url + "/" + Drupal.settings.cgk_sso + "/4", function (data) {
+
+                });
+            });
         }
     };
 })(jQuery);
