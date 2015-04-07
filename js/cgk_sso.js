b1098667a27ab22624c1be16711720fc43ddb7ec
Neal Vanmeert <neal@emuse.be>
2015-04-07 13:46:14 +0200

    Cleaned up JS.

--- js/cgk_sso.js
+++ js/cgk_sso.js
@@ -1,14 +1,7 @@
 (function ($) {
     Drupal.behaviors.cgk_sso = {
         attach: function (context, settings) {
-            console.log(Drupal.settings.cgk_sso.callback_urls);
             $.each(Drupal.settings.cgk_sso.callback_urls, function (key, callback_url) {
-                //$.ajax({
-                //    url: callback_url,
-                //    type: "GET",
-                //    dataType: 'jsonp'
-                //});
-                console.log(callback_url);
                 $('body').append('<iframe src="' + callback_url + '" style="display: none"></iframe>');
 
             });
